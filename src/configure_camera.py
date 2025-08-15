919bc84aec28e69adc7a26767d93f1e1971b0662
Safar Galimzianov <safargalimzyanov@protonmail.com>
2025-08-15 16:59:52 +0500

    add env

--- src/configure_camera.py
+++ src/configure_camera.py
@@ -4,9 +4,8 @@ import socket
 from time import sleep
 import sys
 import os
+import env
 
-SURVEILLANCE_IP_RANGE = 6
-NTP_BAT_NAME = 'ntp.bat'
 
 def ping_ip(ip):
     num_of_requests = 1
@@ -38,28 +37,26 @@ def get_local_ip():
     
 def main():
     local_ip = get_local_ip()
-    if local_ip.split('.')[-2] != str(SURVEILLANCE_IP_RANGE):
+    if local_ip.split('.')[-2] != str(env.SURVEILLANCE_IP_RANGE):
         print('Local IP is not in the expected range')
     
     print('Starting NTP server')
-    base_dir = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__))
-    ntp_path = os.path.join(base_dir, NTP_BAT_NAME)
-    command = ['sudo', 'run', ntp_path]
+    command = ['sudo', 'run', env.NTP_CONFIG_PATH]
     result = 0
     try:
         result = subprocess.run(command, stdout=subprocess.DEVNULL).returncode
+    except Exception as e:
+        print(f'Error starting NTP server: {e}')
+    else:
         if result == 0:
             # user gets prompted to start admin command
             # and this is printed
             print('Successfull start of NTP server')
         else:
             print('Failed to start NTP server')
-    except Exception as e:
-        print(f'Error starting NTP server: {e}')
-    sleep(3)
-    
+
     print('Starting ping check')
-    with open('camera_list.json', 'r', encoding='utf-8') as f:
+    with open(env.DATA_PATH, 'r', encoding='utf-8') as f:
         cameras = json.load(f)
         for camera in cameras:
             name = str(camera.get('Name')).strip()
